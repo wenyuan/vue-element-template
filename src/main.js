5e3717a17ffeb01a2d6284b5f1f4b9d4c51ecce2
wenyuan <xuwenyuan2016@163.com>
2020-11-27 11:51:53 +0800

    feat: update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import * as filters from './filters' // global filters
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
