fc4d08b8e2e927ee01b3e81ed38f79118664c41a
wenyuan <xuwenyuan2016@163.com>
2020-11-27 13:22:28 +0800

    feat: add plugin: element-ui and normalize.css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
+
+// A modern alternative to CSS resets
+import 'normalize.css/normalize.css'
+
+// Element
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// Global CSS
+import '@/styles/index.scss'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import * as filters from './filters' // global filters
+
+Vue.use(ElementUI, {
+  size: 'medium' // set element-ui default size
+})
 
 // register global utility filters
 Object.keys(filters).forEach(key => {
